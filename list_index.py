bc66ebebe3a42613067f9c61ead9e044d80499b9
NageswaraRao <pythontrainernag@gmail.com>
2020-02-18 07:31:56 +0530

    18/02/200

---
+++ list_index.py
@@ -0,0 +1,6 @@
+a = [3, 4, 2, 10, 5, 7, 8, 10, 2, 12, 2, 15, 17, 2, 2, 19, 20]
+for x in range(len(a)):
+    if a[x] == 2:
+        print("the first occurance of 2 is at index", x)
+        break
+    
